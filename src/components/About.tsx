126e9b86998a8d52c2abe4a14c6dbde0a160fa03
Jonathan White <contact@jonathancwhite.com>
2023-10-02 11:28:21 -0500

    added variables file and updated about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,52 +3,57 @@ import React from "react";
 function About() {
 	return (
 		<>
-			<div className="about_container">
-				<div className="about ">
-					<div className="text">
+			<div className='about_container'>
+				<div className='about '>
+					<div className='text'>
 						<h2>About.</h2>
 						<p>We are xmplfy.</p>
 						<br></br>
-						<div className="na_stand">
+						<div className='na_stand'>
 							<p>
-								Xmplfy is more than an esports organization; we
-								are dedicated to our players and our nation. We,
-								as an organization, put our players and creators
-								first while supporting where we are from; we
-								embrace North America as part of us, and we are
-								here to stay, hoping to provide and create the
-								North American stars we have seen before! From
-								the start of this organization, we had North
-								America in our site, and we, as an organization,
-								will help our players grow inside and outside of
-								the game, building up their brand as well. We
-								love esports, but we do know the hardships that
-								happen when you reach a certain level. xmplfy is
-								here to mentor and provide support to North
-								American players and content creators as we
-								create unique shows to entertain while keeping
-								our eyes on tournaments and leagues for our
-								teams!
+								At our core, Xmplfy is more than just an esports
+								organization – we are a North American movement.
+								Born and bred on this continent, we carry the
+								spirit and tenacity of North America in
+								everything we do. Our commitment? To raise the
+								banner of North American esports higher than
+								ever before.
 								<br></br>
 								<br></br>
-								We aren't your regular organization; we make
-								sure everyone of our players, content creators,
-								designers, editors, and staff is treated like
-								family. As we grow, we often ask what we want it
-								to look like. We love the opportunities we have
-								to work with some of North America's rising
-								stars, and we will always feel like they are
-								part of our family we have here at xmplfy. We
-								will always have our family's backs, which means
-								we will make sure that they get the best!
+								From day one, North America wasn't just our
+								target – it was our essence. Our focus is to
+								champion the North American stars of yesterday,
+								today, and tomorrow. We are wholeheartedly
+								devoted to nurturing, mentoring, and propelling
+								our players and creators to the zenith of their
+								potential, both in-game and beyond.
 								<br></br>
 								<br></br>
-								As we embark on our journey into 2024, there
-								will be a lot more opportunities that come our
-								way. We are driven to help and pave the way for
-								NA! We will be part of North America's growth
-								and want to keep these roots strong. We are
-								North America. We are Xmplfy! 
+								Esports is our passion, and we recognize the
+								challenges players face as they ascend the
+								ranks. This is why Xmplfy exists – to be the
+								guiding light and steadfast support for every
+								North American player and content creator. We're
+								here to not only participate in tournaments and
+								leagues but to set the stage alight with unique
+								entertainment shows.
+								<br />
+								<br />
+								Xmplfy isn't just an organization; it's a
+								family. Every player, content creator, designer,
+								editor, and staff member is embraced as an
+								integral part of our North American family. We
+								celebrate the privilege of collaborating with
+								North America's rising talents and assure them
+								that here, at Xmplfy, they will always find a
+								home.
+								<br />
+								<br />
+								As 2024 beckons, the horizon brims with
+								opportunities. Our vision is clear – to be the
+								torchbearers for North American esports. Our
+								roots are deep, our resolve unshakeable. We are
+								North America's pulse. We are Xmplfy!
 							</p>
 						</div>
 					</div>
