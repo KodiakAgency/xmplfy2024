6a244fff0e2424b969156140057a4972b8995f83
Jonathan White <contact@jonathancwhite.com>
2023-10-02 16:19:26 -0500

    added player cards and csgo page

---
+++ src/components/PlayerCard.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { RiTwitterXFill, RiTwitchFill, RiYoutubeFill } from "react-icons/ri";
+
+const PlayerCard = ({
+	Position,
+	Alias,
+	Name,
+	x_link,
+	stream_link,
+	youtube_link,
+}) => {
+	return (
+		<div className='playerCard'>
+			<div className='playerCard__image'></div>
+			<div className='playerCard__details'>
+				<div className='playerCard__details--top'>
+					<h4 className='playerCard__position blue'>{Position}</h4>
+					<h2 className='playerCard__alias'>{Alias}</h2>
+					<h4 className='playerCard__name'>{Name}</h4>
+				</div>
+				<div className='playerCard__details--bottom'>
+					<a href={x_link} target='_blank'>
+						<RiTwitterXFill />
+					</a>
+					<a href={stream_link} target='_blank'>
+						<RiTwitchFill />
+					</a>
+					<a href={youtube_link} target='_blank'>
+						<RiYoutubeFill />
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default PlayerCard;
--- src/pages/teams/csgo.tsx
+++ src/pages/teams/csgo.tsx
@@ -1,16 +1,92 @@
 import React from "react";
 import Layout from "../../components/Layout";
-import cs2Backing from "../../assets/img/cs2.jpg";
 import Image from "next/image";
+import csChar from "../../assets/img/Team_CS_wht_80x80.svg";
+import PlayerCard from "../../components/PlayerCard";
 
 const csgo = () => {
 	return (
 		<Layout>
 			<section className='teamPage cs2'>
-				<div className='teamPage__cover'>
-					<Image src={cs2Backing} alt='counter strike 2 graphic' />
+				<div className='teamPage__cover'></div>
+				<div className='teamPage__body'>
+					<div className='teamPage__content'>
+						<h2>Counter-Strike 2</h2>
+						<div className='teamPage__content--team'>
+							<Image src={csChar} alt='' />
+							<p>
+								Dating back to Counter-Strike: Global Offensive,
+								xmplfy has had a long standing presence in the
+								counter-strike franchise. Xmplfy's main roster
+								consists of five fresh and hungry players ready
+								to take on the top tiers of talent and bring
+								home a championship for the North American
+								scene. Driven by their dedication and love for
+								competition, Xmplfy CS is determined to rise
+								through the ranks and win it all.
+							</p>
+						</div>
+					</div>
 				</div>
-				<div className='teamPage__body'></div>
+				<div className='teamPage__players'>
+					<h2 className='heading heading--dark'>Players</h2>
+					<div className='teamPage__players--cards'>
+						<PlayerCard
+							Position={"Rifler"}
+							Alias={"Darke9000"}
+							Name={"Austin Turner"}
+							x_link={"https://x.com/darke9k"}
+							stream_link={"https://twitch.tv/darke9k"}
+							youtube_link={"https://youtube.com/@xmplfy"}
+						/>
+						<PlayerCard
+							Position={"Rifler"}
+							Alias={"nrb"}
+							Name={"Nicholas Brugna"}
+							x_link={"https://x.com/nrbcs"}
+							stream_link={"https://twitch.tv/nrbcs"}
+							youtube_link={"https://youtube.com/@xmplfy"}
+						/>
+						<PlayerCard
+							Position={"Rifler"}
+							Alias={"Hoozh"}
+							Name={"Aidan Labaschin"}
+							x_link={"https://x.com/hoozhur"}
+							stream_link={"https://twitch.tv/hoozhur"}
+							youtube_link={"https://youtube.com/@xmplfy"}
+						/>
+						<PlayerCard
+							Position={"Rifler"}
+							Alias={"Girthquake"}
+							Name={"Matt Locher"}
+							x_link={"https://x.com/GirthquakeCS"}
+							stream_link={"https://twitch.tv/girthquakecs"}
+							youtube_link={"https://youtube.com/@xmplfy"}
+						/>
+						<PlayerCard
+							Position={"Rifler"}
+							Alias={"Jynx"}
+							Name={"Mark Coyu"}
+							x_link={"https://x.com/1jynx"}
+							stream_link={"https://twitch.tv/1jynx"}
+							youtube_link={"https://youtube.com/@xmplfy"}
+						/>
+					</div>
+				</div>
+
+				<div className='teamPage__players'>
+					<h2 className='heading heading--dark'>Support Staff</h2>
+					<div className='teamPage__players--cards'>
+						<PlayerCard
+							Position={"Manager"}
+							Alias={"rEqN_"}
+							Name={"Some Name"}
+							x_link={"https://x.com/xmplfy"}
+							stream_link={"https://twitch.tv/xmplfygg"}
+							youtube_link={"https://youtube.com/@xmplfy"}
+						/>
+					</div>
+				</div>
 			</section>
 		</Layout>
 	);
