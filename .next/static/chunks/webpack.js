43176d7f2b0d974d9d4b82a7eb70b8a0c5bfcaa3
Joey Chmielewski <joey.chm@comcast.net>
2023-10-01 19:47:27 -0700

    remove sticky header

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "83418d4895a21fb0"; }
+/******/ 		__webpack_require__.h = function() { return "183c3d84ed024726"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
