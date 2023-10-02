5db60bbbc052b6afa0c74e77b85c71a5bc3478af
Joey Chmielewski <joey.chm@comcast.net>
2023-10-01 19:45:22 -0700

    fixed issues with @
    not sure why it had an issue with using the @ but it did fixed.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "83ed42d6272fb79b"; }
+/******/ 		__webpack_require__.h = function() { return "83418d4895a21fb0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
-import Header from "@/components/Header";
-import HomePage from "@/components/HomePage";
-import Footer from "@/components/Footer";
+import Header from "../components/Header";
+import HomePage from "../components/HomePage";
+import Footer from "../components/Footer";
 
 export default function Home() {
 	return (
