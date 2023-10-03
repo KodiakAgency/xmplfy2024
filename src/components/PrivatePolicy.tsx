8cce188d9cacd64f23e245ea819818759ddf1f4a
Joey Chmielewski <joey.chm@comcast.net>
2023-10-02 17:29:48 -0700

    updates for Private Policy page

---
+++ src/components/PrivatePolicy.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PrivatePolicy() {
+	return <div>PrivatePolicy</div>;
+}
+
+export default PrivatePolicy;
---
+++ src/pages/privacy.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../components/Layout";
+import PrivatePolicy from "../components/PrivatePolicy";
+
+function privacy() {
+	return (
+		<>
+			<Layout>
+				<PrivatePolicy />
+			</Layout>
+		</>
+	);
+}
+
+export default privacy;
