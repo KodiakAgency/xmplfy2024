f0c63ae9c2c94103fea8dc84e2236e6e76b98326
Joey Chmielewski <joey.chm@comcast.net>
2023-10-02 18:08:23 -0700

    Privacy page

--- src/components/PrivatePolicy.tsx
+++ src/components/PrivatePolicy.tsx
@@ -1,7 +1,148 @@
 import React from "react";
 
 function PrivatePolicy() {
-	return <div>PrivatePolicy</div>;
+	return (
+		<>
+			<div className="private_container">
+				<div className="private">
+					<h2 className="grey">PRIVACY POLICY</h2>
+					<p className="grey">
+						This Privacy Policy describes how xmplfy.com (the “Site”
+						or “we”) collects, uses, and discloses your Personal
+						Information when you vist the site.
+					</p>
+
+					<h3 className="grey">Contact</h3>
+					<p className="grey">
+						After reviewing this policy, if you have additional
+						questions, want more information about our privacy
+						practices, or would like to make a complaint, please
+						contact us by e-mail at jonathan@builtbykodiak.com or
+						joseph@builtbykodiak.com
+					</p>
+
+					<h3 className="grey">COLLECTING PERSONAL INFORMATION</h3>
+					<p className="grey">
+						When you visit the Site, we collect certain information
+						about your device, your interaction with the Site, and
+						information necessary to process your purchases. We may
+						also collect additional information if you contact us
+						for customer support. In this Privacy Policy, we refer
+						to any information about an identifiable individual
+						(including the information below) as “Personal
+						Information”. See the list below for more information
+						about what Personal Information we collect and why.
+					</p>
+
+					<h3 className="grey">Device information</h3>
+					<p className="grey">
+						- Purpose of collection: to load the Site accurately for
+						you, and to perform analytics on Site usage to optimize
+						our Site.
+						<br></br>- Source of collection: Collected automatically
+						when you access our Site using cookies, log files, web
+						beacons, tags, or pixels. <br></br>- Disclosure for a
+						business purpose: shared with our processor Shopify,
+						Meta, and Google. <br></br>- Personal Information
+						collected: version of web browser, IP address, time
+						zone, cookie information, what sites or products you
+						view, search terms, and how you interact with the Site.
+					</p>
+
+					<h3 className="grey">MINORS</h3>
+					<p className="grey">
+						The Site is not intended for individuals under the age
+						of 13. We do not intentionally collect Personal
+						Information from children. If you are the parent or
+						guardian and believe your child has provided us with
+						Personal Information, please contact us at the address
+						above to request deletion.
+					</p>
+					<h3 className="grey">MINORS</h3>
+					<p className="grey">
+						The Site is not intended for individuals under the age
+						of 13. We do not intentionally collect Personal
+						Information from children. If you are the parent or
+						guardian and believe your child has provided us with
+						Personal Information, please contact us at the address
+						above to request deletion.
+					</p>
+
+					<h3 className="grey">BEHAVIORAL ADVERTISING</h3>
+					<p className="grey">
+						As described above, we use your Personal Information to
+						provide you with targeted advertisements or marketing
+						communications we believe may be of interest to you. For
+						example: - We use Google Analytics to help us understand
+						how our customers use the Site. You can read more about
+						how Google uses your Personal Information here:
+						https://www.google.com/intl/en/policies/privacy/. You
+						can also opt-out of Google Analytics here:
+						https://tools.google.com/dlpage/gaoptout.
+					</p>
+
+					<h3 className="grey">LAWFUL BASIS</h3>
+					<p className="grey">
+						Pursuant to the General Data Protection Regulation
+						(“GDPR”), if you are a resident of the European Economic
+						Area (“EEA”), we process your personal information under
+						the following lawful bases: <br></br>-Your consent;{" "}
+						<br></br>- The performance of the contract between you
+						and the Site; <br></br>- Compliance with our legal
+						obligations; <br></br>- To protect your vital interests;{" "}
+						<br></br>- To perform a task carried out in the public
+						interest; <br></br>- For our legitimate interests, which
+						do not override your fundamental rights and freedoms.
+					</p>
+
+					<h3 className="grey">CCPA</h3>
+					<p className="grey">
+						If you are a resident of California, you have the right
+						to access the Personal Information we hold about you
+						(also known as the ‘Right to Know’), to port it to a new
+						service, and to ask that your Personal Information be
+						corrected, updated, or erased. <br></br> If you would
+						like to exercise these rights, please contact us through
+						the contact information above. If you would like to
+						designate an authorized agent to submit these requests
+						on your behalf, please contact us at the address above.
+					</p>
+
+					<h3 className="grey">COOKIES</h3>
+					<p className="grey">
+						A cookie is a small amount of information that’s
+						downloaded to your computer or device when you visit our
+						Site. We use a number of different cookies, including
+						functional, performance, advertising, and social media
+						or content cookies. Cookies make your browsing
+						experience better by allowing the website to remember
+						your actions and preferences (such as login and region
+						selection). This means you don’t have to re-enter this
+						information each time you return to the site or browse
+						from one page to another. Cookies also provide
+						information on how people use the website, for instance
+						whether it’s their first time visiting or if they are a
+						frequent visitor.
+						<br></br>
+						We use the following cookies to optimize your experience
+						on our Site and to provide our services.
+					</p>
+
+					<h3 className="grey">CHANGES</h3>
+					<p className="grey">
+						We may update this Privacy Policy from time to time in
+						order to reflect, for example, changes to our practices
+						or for other operational, legal, or regulatory reasons.
+					</p>
+					<h3 className="grey">COMPLAINTS</h3>
+					<p className="grey">
+						As noted above, if you would like to make a complaint,
+						please contact us by e-mail.
+					</p>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default PrivatePolicy;
